d4cf9bdc6a7a69e99fd027117578c50908efbb9b
NathSanFer <158168917+Nathsanfer@users.noreply.github.com>
2025-11-18 09:14:07 -0300

    feat: create color variables and apply fonts

--- src/app/(rotas)/home/page.jsx
+++ src/app/(rotas)/home/page.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./home.module.css";
 
-export default function home() {
+export default function Home() {
   return (
     
     <div>
-      <h1>Hello!!</h1>
+      <h1 className={styles.title}>Hello!!</h1>
     </div>
   );
 }
