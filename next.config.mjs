80091d10a128425771843a464b04259d45e98dcf
F3rNnd4 <fernandaalves.tds2.senai@gmail.com>
2025-11-27 11:42:29 -0300

    feat: add image processing logic and adjusts image settings

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,28 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5001',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '4001',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3001',
+        pathname: '/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
